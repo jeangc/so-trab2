21697975edb30175dbbe21cddb04ac2bee18cf39
Jean Gonçalves <jean.gnc@gmail.com>
2017-12-01 00:10:44 -0200

    Cria uma classe de configuração

---
+++ src/br/ufsc/Config.java
@@ -0,0 +1,13 @@
+package br.ufsc;
+
+class Config {
+    // running configs
+    static final String[] TEAMS = {"Amarelo", "Verde", "Azul"};
+    static final int DOGS_PER_TEAM = 2;
+    static final int MAXIMUM_PARALLEL_DOGS = 1;
+    static final int TIME_UNIT_MILLISECONDS = 100;
+
+    // limits
+    static final int MAXIMUM_DOG_COINS = 20;
+    static final int REQUIRED_COINS_WINNER = 50;
+}
--- src/br/ufsc/Main.java
+++ src/br/ufsc/Main.java
@@ -4,15 +4,11 @@ import java.util.concurrent.*;
 
 
 public class Main {
-    private static final String[] TEAMS = {"Amarelo", "Verde", "Azul"};
-    private static final int DOGS_PER_TEAM = 2;
-    private static final int MAXIMUM_PARALLEL_DOGS = 1;
-
     public static void main(String[] args) {
-        for(String t: TEAMS){
-            ExecutorService executorService = Executors.newFixedThreadPool(MAXIMUM_PARALLEL_DOGS);
+        for(String t: Config.TEAMS){
+            ExecutorService executorService = Executors.newFixedThreadPool(Config.MAXIMUM_PARALLEL_DOGS);
 
-            for (int i = 0; i < DOGS_PER_TEAM; i++) {
+            for (int i = 0; i < Config.DOGS_PER_TEAM; i++) {
                 executorService.execute(new Dog() {
                     public void run() {
                         int j = 0;
